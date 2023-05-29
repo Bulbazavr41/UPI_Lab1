69b8fefbd5bf15e8b449c2b7cc5d5d672e5f0a40
Bulbazavr41 <72260541+Bulbazavr41@users.noreply.github.com>
2023-05-29 22:09:30 +0300

    Решено первое задание

---
+++ Lab1.v0.5.py
@@ -0,0 +1,23 @@
+def charToUpper(char):
+    if len(char) > 1:
+        print("Ошибка, введите ровно 1 символ")
+    elif char >= 'a' and char <= 'z':
+        return chr(ord(char) - 32)
+    else:
+        return char
+    
+while 1:
+    try:
+        option = int(input("Введите номер задачи. Для выхода используйте любое число меньше 0: "))
+    except ValueError:
+        print("Нужна циферка")
+    if option == 1:
+        print("Задание 1")
+        char = charToUpper(input("Введите символ: "))
+        print(char)
+    elif option == 2:
+        print("Задание 2")
+    elif option < 0:
+        break
+    else:
+        print("Всего 2 задачи")  
